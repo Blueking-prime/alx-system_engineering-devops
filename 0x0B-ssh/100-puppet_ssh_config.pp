47ed0bb735906026f611e27f790b804ce6bddd14
Nnamdi Anyanwu <blueking189@gmail.com>
2022-10-16 22:16:37 +0100

    update puppet file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,12 +5,10 @@ file_line { 'Declare identity file':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => '    IdentityFile ~/.ssh/school',
-  match  => 'IdentityFile'
 }
 
 file_line { 'Turn off passwd auth':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => '    BatchMode yes',
-  match  => 'BatchMode'
 }
